10602ac511ee0c712aa23358d42e887362d7abab
Raghvendra Jain <raghavendra.jain@gmail.com>
2019-03-15 21:19:20 +0900

    first

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/file/d/1PWu0Zut_5LrUJiBe8vLZBftBq91MxpqM/view?usp=sharing'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
